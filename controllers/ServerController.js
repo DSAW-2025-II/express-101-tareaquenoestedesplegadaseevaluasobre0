98abcd9e6a247b7f756537d3e043918b976404dd
Martin Polo Santini <polosantinimartin@gmail.com>
2025-09-18 21:47:43 -0500

    Update ServerController.js

--- controllers/ServerController.js
+++ controllers/ServerController.js
@@ -3,7 +3,7 @@ module.exports = {
     
     //Get que envía un mensaje como respuesta
     get : (request, response) => {
-        response.send(`Hello, take into account that this team has ${process.env.ID_TOTAl} members`)
+        response.send(`Hello, take into account that this team has ${process.env.ID_TOTAL} members`)
     }
     
 }
